b70615699740e393738f67c82c7b2d4b4fc34087
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-08-23 09:55:57 +0200

    add hamburger menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import logo from "./../assets/group-15.svg"
 import heroArt from "./../assets/untitled-artwork-7-1@2x.png";
+import CloseIcon from "./../assets/close-circl.svg";
+import MenuIcon from "./../assets/menu.svg";
 
 
 const Hero = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const clickHandlerMenu = () => {
+    return setShowMenu(!showMenu);
+  }
+
   return (
     <>
       {/* Hero Section */}
       <section className="relative h-[82dvh] overflow-hidden">
         <div className="container">
-          
           {/* Header */}
           <header className="flex justify-between xl:px-[8rem] px-4 pt-4 md:pt-8">
-
             {/* Company Logo */}
             <div>
               <img src={logo} alt="Company Logo" />
@@ -38,6 +45,47 @@ const Hero = () => {
                 Work with us!
               </a>
             </nav>
+
+            <button
+              onClick={clickHandlerMenu}
+              className="p-0 m-0 bg-transparent border-0 md:hidden"
+            >
+              <img
+                className="w-10 h-10 rounded-full "
+                src={showMenu ? CloseIcon : MenuIcon}
+                alt={showMenu ? "close" : "menu"}
+              />
+            </button>
+
+            {showMenu && (
+              <div className="fixed z-40 inset-1 top-20">
+                <nav className="flex flex-col items-center gap-12 py-16 mb-3 font-normal text-white shadow-xl bg-darkslategray/60 rounded-3xl backdrop-blur-md ">
+                  <a
+                    className="no-underline transition-all hover:text-coral hover:font-medium"
+                    href="#Startups"
+                    onClick={clickHandlerMenu}
+                  >
+                    Startups
+                  </a>
+                  <a
+                    className="no-underline transition-all hover:text-coral hover:font-medium"
+                    href="#Contact"
+                    onClick={clickHandlerMenu}
+                  >
+                    Contact
+                  </a>
+                  <a
+                    href="#Work"
+                    onClick={clickHandlerMenu}
+                    className="px-10 py-3 font-bold no-underline bg-white rounded-full ring-1 ring-complementary-green text-complementary-green tracking-wider hover:-translate-y-0.5 transition-all hover:shadow-md"
+                  >
+                    Work with us!
+                  </a>
+                </nav>
+              </div>
+            )}
+
+            {}
           </header>
 
           {/* Content */}
